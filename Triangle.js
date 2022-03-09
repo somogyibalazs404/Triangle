40f5c09aca3fde8688347f6d24b8e3663a3a3399
Balazs Somogyi <4raptor4@gmail.com>
2022-03-09 21:07:45 +0100

    Triangle

--- Triangle.js
+++ Triangle.js
@@ -1,23 +1,23 @@
 let a = Math.round((Math.random() + 1) * 25);
 let b = Math.round((Math.random() + 1) * 25);
-let c = Math.round((Math.random() + 1) * 25)
+let c = Math.round((Math.random() + 1) * 25c =)
 let cb = c + b;
 let ab = a + b;
 let ac = a + c;
 if (ab > c && cb > a && ac > b) {
-    document.write(`Your triangle is ${a} x ${b} x ${c}, it is an existing triangle.`);
+    document.write(`Your triangle is a=${a} x b=${b} x c=${c}, it is an existing triangle.`);
 }
 else if (ab > c && cb > a && ac < b) {
-    document.write(`Your triangle is ${a} x ${b} x ${c}, it's "b" side is too long so it does not exist, because "a" and "b" is ${ac} which is shorter than ${b}`);
+    document.write(`Your triangle is a=${a} x b=${b} x c=${c}, it's "b" side is too long so it does not exist, because "a" and "b" is ${ac} which is shorter than ${b}`);
 }
 else if (ab > c && cb < a && ac > b) {
-    document.write(`Your triangle is ${a} x ${b} x ${c}, it's "a" side is too long so it does not exist, because "a" and "b" is ${cb} which is shorter than ${a}`);
+    document.write(`Your triangle is a=${a} x b=${b} x c=${c}, it's "a" side is too long so it does not exist, because "a" and "b" is ${cb} which is shorter than ${a}`);
 }
 else if (ab < c && cb > a && ac > b) {
-    document.write(`Your triangle is ${a} x ${b} x ${c}, it's "c" side is too long so it does not exist, because "a" and "b" is ${ab} which is shorter than ${c}.`);
+    document.write(`Your triangle is a=${a} x b=${b} x c=${c}, it's "c" side is too long so it does not exist, because "a" and "b" is ${ab} which is shorter than ${c}.`);
 }
 else if (ab == c | cb == a | ac == b) {
-    document.write(`Your triangle is ${a} x ${b} x ${c}, it is an existing triangle, non other than a isosceles triangle.`);
+    document.write(`Your triangle is a=${a} x b=${b} x c=${c}, it is an existing triangle, non other than a isosceles triangle.`);
 }
 else {
     document.write(`Ilyen nincs! ${a} ${b} ${c}`)
