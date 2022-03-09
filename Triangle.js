b4186e69911184454945337752f854e06ae36f29
Balazs Somogyi <4raptor4@gmail.com>
2022-03-09 20:43:49 +0100

    Triangle

---
+++ Triangle.js
@@ -0,0 +1,24 @@
+let a = Math.round((Math.random()) * 50);
+let b = Math.round((Math.random()) * 50);
+let c = Math.round((Math.random()) * 50)
+let cb = c + b;
+let ab = a + b;
+let ac = a + c;
+if (ab > c && cb > a && ac > b) {
+    document.write(`Your triangle is ${a} x ${b} x ${c}, it is an existing triangle.`);
+}
+else if (ab > c && cb > a && ac < b) {
+    document.write(`Your triangle is ${a} x ${b} x ${c}, it's "b" side is too long so it does not exist, because "a" and "b" is ${ac} which is shorter than ${b}`);
+}
+else if (ab > c && cb < a && ac > b) {
+    document.write(`Your triangle is ${a} x ${b} x ${c}, it's "a" side is too long so it does not exist, because "a" and "b" is ${cb} which is shorter than ${a}`);
+}
+else if (ab < c && cb > a && ac > b) {
+    document.write(`Your triangle is ${a} x ${b} x ${c}, it's "c" side is too long so it does not exist, because "a" and "b" is ${ab} which is shorter than ${c}.`);
+}
+else if (ab == c | cb == a | ac == b) {
+    document.write(`Your triangle is ${a} x ${b} x ${c}, it is an existing triangle, non other than a right-angle triangle.`);
+}
+else {
+    document.write(`Ilyen nincs! ${a} ${b} ${c}`)
+}
